713072e5db6a9b0a0902b5888667646a426778cc
Cristofer Flores <cristofer.flort@gmail.com>
2018-03-17 08:51:00 -0600

    Added items dynamically to the burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,15 +4,19 @@ import './Burger.scss';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  const burgerIngredients = Object.keys(props.ingredients)
+    .map( key => {
+      return [...Array(props.ingredients[key])].map((_, i) => {
+        return <BurgerIngredient key={key + i} type={key}/>
+      })
+    })
+  
   return (
     <div id="BurgerContainer">
       <div className="burger">
-        <BurgerIngredient type="bread-top"/>
-        <BurgerIngredient type="cheese"/>
-        <BurgerIngredient type="meat"/>
-        <BurgerIngredient type="salad"/>
-        <BurgerIngredient type="bacon"/>
-        <BurgerIngredient type="bread-bottom"/>
+      <BurgerIngredient type="bread-top"/>
+      {burgerIngredients}
+      <BurgerIngredient type="bread-bottom"/>
       </div>
     </div>
   )
