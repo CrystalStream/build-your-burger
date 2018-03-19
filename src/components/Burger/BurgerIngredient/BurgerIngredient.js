277ba222f377f8b2417faaa0dc0ec418f97cfb76
Cristofer Flores <cristofer.flort@gmail.com>
2018-03-19 12:32:52 -0600

    Changed the folder structure to plce the Layout component into the hoc folder

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import Ingredient from '../../../hoc/Ingredient';
+import Ingredient from '../../../hoc/Ingredient/Ingredient';
 import './BurgerIngrediente.scss';
 
 class BurgerIngredient extends Component {
