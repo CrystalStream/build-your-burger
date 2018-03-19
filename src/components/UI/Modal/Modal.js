3df5e5f3be8eee995e50064ad577c86a4f624af1
Cristofer Flores <cristofer.flort@gmail.com>
2018-03-19 12:21:40 -0600

    Improve the application by checking the shouldComponentUpdate lifecycle in the modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
 
 import './Modal.scss';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.closeModal}/>
-  <div className={props.show ? 'Modal is-open' : 'Modal is-closed'}>
-    {props.children}
-  </div>
-  </Aux>
-)
+class Modal extends Component {
 
-export default modal;
+  shouldComponentUpdate = (nextProps, nextState) => {
+    return nextProps.show !== this.props.show
+  }
+  
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.closeModal}/>
+        <div className={this.props.show ? 'Modal is-open' : 'Modal is-closed'}>
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+}
+
+export default Modal
