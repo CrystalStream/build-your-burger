b5aacf5e3261f8ab83ab18be098824655c019a74
Cristofer Flores <cristofer.flort@gmail.com>
2018-03-29 11:06:05 -0600

    Modified the input component to work with two way data binding and send everything to firebase with a click handler for now, still missing submit the form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,16 +5,26 @@ import './Input.scss';
 export default (props) => {
   let formElement = null;
 
-  switch (props.type) {
+  switch (props.elemType) {
     case 'text':
-      formElement = <input className="form-element" {...props}/>
+      formElement = <input className="form-element" {...props.config} onChange={props.changed}/>
       break;
     case 'textarea': 
-      formElement = <textarea className="form-element" {...props}></textarea>
+      formElement = <textarea className="form-element" {...props.config} onChange={props.changed}></textarea>
+      break;
+
+    case 'select': 
+      formElement = (
+        <select className="form-element" {...props.config} onChange={props.changed}>
+          { props.config.options.map( opt => (
+            <option key={opt.value} value={opt.value}>{opt.displayValue}</option>
+          ))}
+        </select>
+      ) 
       break;
   
     default:
-      formElement = <input className="form-element" {...props}/>
+      formElement = <input className="form-element" {...props.config} onChange={props.changed}/>
       break;
   }
   return (
--- src/containers/Checkout/ConctadData/ContactData.js
+++ src/containers/Checkout/ConctadData/ContactData.js
@@ -9,21 +9,57 @@ import './ContactData.scss';
 
 export default class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elemType: 'input',
+        opts: { type: 'text', placeholder: 'Enter your name', name: 'name', value: ''}
+      },
+      street: {
+        elemType: 'input',
+        opts: { type: 'text', placeholder: 'Enter you street', name: 'street', value: ''}
+      },
+      zipcode: {
+        elemType: '',
+        opts: { type: '', placeholder: 'Enter you Zip code', name: 'zipcode', value: ''}
+      },
+      country: {
+        elemType: 'input',
+        opts: { type: 'text', placeholder: 'Enter you country', name: 'country', value: ''}
+      },
+      email: {
+        elemType: 'email',
+        opts: { type: 'email', placeholder: 'Enter you email', name: 'email', value: ''}
+      },
+      deliverymethod: {
+        elemType: 'select',
+        opts: { type: 'text', options: [
+          { value: 'fastest', displayValue: 'Let Flash bring that to me! (he would really like a good tip!)'},
+          { value: 'cheapest', displayValue: 'i\'ll be preparing for a movie meanwhile' }
+        ], 
+        name: 'deliverymethod', 
+        value: 'fastest' }
+      }
     },
     loading: false
   }
 
   orderHandler = (e) => {
     e.preventDefault();
+    console.log(this.state);
     this.setState({ loading: true });
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      contactInfo: {
+        name: this.state.orderForm.name.opts.value,
+        email: this.state.orderForm.email.opts.value,
+        deliveryMethod: this.state.orderForm.deliverymethod.opts.value,
+        address: {
+          street: this.state.orderForm.street.opts.value,
+          zipCode: this.state.orderForm.zipcode.opts.value,
+          country: this.state.orderForm.country.opts.value,
+        }
+      }
     }
     axios.post('/orders.json', order)
     .then(res => {
@@ -37,14 +73,26 @@ export default class ContactData extends Component {
     })
   }
 
+  inputFormChangedHanlder = (e, inpId) => {
+    const orderForm = { ...this.state.orderForm };
+    const elemnt = { ...orderForm[inpId] }
+    const configElem = { ...elemnt.opts }
+    configElem.value = e.target.value;
+    elemnt.opts = configElem
+    orderForm[inpId] = elemnt;
+    this.setState({ orderForm: orderForm })
+  }
+
   render() {
+    let inputs = Object.keys(this.state.orderForm)
+      .map( i => this.state.orderForm[i])
+      .map( (inp, i) => <Input key={inp.opts.name + i} 
+          changed={(e) => this.inputFormChangedHanlder(e, inp.opts.name) } 
+          elemType={inp.elemType} 
+          config={inp.opts}/>)
     let content = (
       <form action="">
-        <Input type="text" name="name" placeholder="Enter your name"/>
-        <Input type="email" name="email" placeholder="Enter your email"/>
-        <Input type="text" name="street" placeholder="Enter your street"/>
-        <Input type="text" name="postalCode" placeholder="Enter your postal code"/>
-
+        {inputs}
         <Button btnType="success" clicked={this.orderHandler}>Order</Button>
       </form>
     );
