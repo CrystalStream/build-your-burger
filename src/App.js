feae8a888bdb870730e19709ba499d09a85a07f0
Cristofer Flores <cristofer.flort@gmail.com>
2018-04-15 09:38:19 -0500

    Added routes guards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom'
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import Layout from './hoc/Layout/Layout';
@@ -16,25 +16,41 @@ class App extends Component {
   }
   
   render() {
+    let routes = (
+      <Switch>
+        <Route path='/login' component={Auth}/>
+        <Route exact path='/' component={BurgerBuilder}/>
+        <Redirect to="/" />
+      </Switch>
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
+      <Switch>
+        <Route path='/checkout' component={Checkout}/>
+        <Route path='/orders' component={Order}/>  
+        <Route path='/logout' component={Logout}/>
+        <Route exact path='/' component={BurgerBuilder}/>
+      </Switch>
+      )
+    }
     return (
     <div>
       <Layout>
-        <Switch>
-          <Route path='/checkout' component={Checkout}/>
-          <Route path='/orders' component={Order}/>
-          <Route path='/login' component={Auth}/>
-          <Route path='/logout' component={Logout}/>
-          <Route exact path='/' component={BurgerBuilder}/>
-        </Switch>
+        {routes}
       </Layout>
     </div>
   );
   }
 }
 
+const mapStateToProps = (state) => ({
+  isAuthenticated: !!state.auth.token
+})
+
 const mapDispatchToProps = {
   onTryAutoSignIn: actions.checkAuthState
 }
 
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
